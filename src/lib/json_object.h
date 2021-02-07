1066e6001e4e848196af9386b2bbeebcfd65d4e9
Creepsy <Robin.gd@outlook.de>
2021-02-07 22:28:43 +0100

    added some declarations

--- src/lib/json_object.h
+++ src/lib/json_object.h
@@ -3,25 +3,49 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <cstdint>
 
-enum value_type {
-    STR,
-    MAP,
-    VEC,
-    NUM,
-    BOOL,
-    NONE
-};
+namespace json {
+    namespace objects {
+        enum value_type {
+            STR,
+            MAP,
+            VEC,
+            INT_NUM,
+            FP_NUM,
+            BOOL,
+            NONE
+        };
 
-union json_value {
-    std::string str;
-    std::map<std::string, json_value> map;
-    std::vector<json_value> vec;
-    double num;
-    bool boolean;
-};
+        struct json_object;
 
-struct json_object {
-    value_type object_type;
-    json_value val;
-};
+        union json_value {
+            std::string str;
+            std::map<std::string, json_object> map;
+            std::vector<json_object> vec;
+            double fp_number;
+            int64_t i_number;
+            bool boolean;
+        };
+
+        struct json_object {
+            value_type object_type;
+            json_value val;
+
+            std::string& get_string();
+            std::map<std::string, json_object>& get_map();
+            std::vector<json_object> get_array();
+            double get_double();
+            int64_t get_int();
+            bool get_bool();
+
+            bool is_string();
+            bool is_map();
+            bool is_array();
+            bool is_double();
+            bool is_int();
+            bool is_bool();
+            bool is_null();
+        };
+    }
+}
