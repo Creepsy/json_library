76cb85197792f053a51b30021a0b27c88ebdb4a7
Creepsy <Robin.gd@outlook.de>
2021-02-08 17:46:00 +0100

    added test

--- src/test.cpp
+++ src/test.cpp
@@ -5,5 +5,10 @@
 int main() {
     json::objects::json_object object{json::objects::value_type::VEC};
 
+    object.get_array().push_back(json::objects::json_object{json::objects::value_type::INT_NUM});
+    object.get_array().at(0).get_int() = 10;
+
+    std::cout << object.get_array().at(0).get_int() << std::endl;
+
     return 0;
 }
