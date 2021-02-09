4c815f5b15bd8a1580e5478cf72cd48a5f4492ab
Creepsy <Robin.gd@outlook.de>
2021-02-09 11:41:43 +0100

    added pretty printing(WIP)

--- src/lib/json.cpp
+++ src/lib/json.cpp
@@ -2,6 +2,60 @@
 
 #include <system_error>
 
+//private helper functions
+
+std::ostream& write_intendations(std::ostream& stream, size_t intendation_count) {
+    for(size_t i = 0; i < intendation_count; i++) {
+        stream << '\t';
+    }
+    return stream;
+}
+
+std::ostream& write_json_object(std::ostream& stream, json::json_object& obj, size_t intendation_count) {
+    switch(obj.object_type) {
+        case json::value_type::STR:
+            return stream << '"' << obj.get_string() << '"';
+        case json::value_type::BOOL:
+            return stream << (obj.get_bool() ? "true" : "false");
+        case json::value_type::FP_NUM:
+            return stream << obj.get_double();
+        case json::value_type::INT_NUM:
+            return stream << obj.get_int();
+        case json::value_type::MAP:
+            stream << "{" << std::endl;
+            {
+                size_t pos = 0;
+                for(std::pair<const std::string, json::json_object>& sub_obj : obj.get_map()) {
+                    if(pos != 0) stream << ", " << std::endl;
+                    write_intendations(stream, intendation_count + 1);
+                    stream << '"' << sub_obj.first << '"' << ": ";
+                    write_json_object(stream, sub_obj.second, intendation_count + 1);
+                    pos++;
+                }    
+            }
+            stream << std::endl;
+            write_intendations(stream, intendation_count);
+            return stream << "}";
+        case json::value_type::VEC:
+            stream << "[" << std::endl;
+            for(size_t i = 0; i < obj.get_array().size(); i++) {
+                if(i != 0) stream << ", " << std::endl;
+                write_intendations(stream, intendation_count + 1);
+                write_json_object(stream, obj.get_array().at(i), intendation_count + 1);
+                
+            }
+            stream << std::endl;
+            write_intendations(stream, intendation_count);
+            return stream << "]";
+        case json::value_type::NONE:
+            return stream << "null";
+    }
+
+    return stream;
+}
+
+
+
 std::string& json::json_object::get_string() {
     if(!this->is_string()) throw std::runtime_error("JSON object isn't a string!");
     return *(std::string*)this->data;
@@ -86,36 +140,5 @@ json::json_object::json_object(value_type object_type) : data{}, object_type{obj
 }
 
 std::ostream& json::operator<<(std::ostream& stream, json_object& obj) {
-    switch(obj.object_type) {
-        case value_type::STR:
-            return stream << '"' << obj.get_string() << '"';
-        case value_type::BOOL:
-            return stream << (obj.get_bool() ? "true" : "false");
-        case value_type::FP_NUM:
-            return stream << obj.get_double();
-        case value_type::INT_NUM:
-            return stream << obj.get_int();
-        case value_type::MAP:
-            stream << "{";
-            {
-                size_t pos = 0;
-                for(std::pair<const std::string, json_object>& sub_obj : obj.get_map()) {
-                    if(pos != 0) stream << ", ";
-                    stream << '"' << sub_obj.first << '"' << " : " << sub_obj.second;
-                   pos++;
-                }    
-            }
-            return stream << "}";
-        case value_type::VEC:
-            stream << "[";
-            for(size_t i = 0; i < obj.get_array().size(); i++) {
-                if(i != 0) stream << ", ";
-                stream << obj.get_array().at(i);
-            }
-            return stream << "]";
-        case value_type::NONE:
-            return stream << "null";
-    }
-
-    return stream;
+    return write_json_object(stream, obj, 0);
 }
--- src/test.cpp
+++ src/test.cpp
@@ -5,12 +5,15 @@
 int main() {
     json::json_object object{json::value_type::MAP};
 
-    object.get_map().insert(std::make_pair("test", json::json_object{json::value_type::VEC}));
-
-    for(int i = 0; i < 10; i++) {
-        object.get_map().at("test").get_array().push_back(json::json_object{json::value_type::INT_NUM});
-        object.get_map().at("test").get_array().at(i).get_int() = i;
+    object.get_map().insert(std::make_pair("outer", json::json_object{json::value_type::VEC}));
+    for(int v = 0; v < 3; v++) {
+        object.get_map().at("outer").get_array().push_back(json::json_object{json::value_type::VEC});
+        for(int i = 0; i < 10; i++) {
+            object.get_map().at("outer").get_array().at(v).get_array().push_back(json::json_object{json::value_type::INT_NUM});
+            object.get_map().at("outer").get_array().at(v).get_array().at(i).get_int() = i;
+        }
     }
+    object.get_map().insert(std::make_pair("is_true", json::json_object{json::value_type::BOOL}));
 
     std::cout << object << std::endl;
 
